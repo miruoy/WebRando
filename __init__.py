e86ab68a2967f264c3e0fb0cefa11e6363e0a4ec
Youri Matthys <2657774+miruoy@users.noreply.github.com>
2026-08-21 20:07:43 +0200

    WebRando: import supybot toegevoegd in __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@
 # with this program; if not, see <https://www.gnu.org/licenses/>.
 
 ### WebRando — plugin entry point
+import supybot
 from . import plugin
 from . import config
 from importlib import reload
